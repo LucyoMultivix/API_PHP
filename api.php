7bcab4f5c243f756b67eba4f77d89260d6215ea5
Lucyo <144945232+LucyoMultivix@users.noreply.github.com>
2025-05-30 14:29:42 -0300

    Add files via upload

---
+++ api.php
@@ -0,0 +1,28 @@
+<?php
+
+header('Content-Type: application/json');
+
+$perfil = [
+    'id' => 1,
+    'nomeCompleto' => 'Lucyo Regini Belloti',
+    'profissao' => 'Estudante de Sistemas de Informação',
+    'email' => 'samangosuado@gmail.com',
+    'senioridade' => 'Junior',
+    'bio' => 'Programador por Hobby'
+];
+
+$caminho = parse_url($_SERVER['REQUEST_URI'],
+PHP_URL_PATH);
+$metodo = $_SERVER['REQUEST_METHOD'];
+$rotaBase = '/api.php'
+$rotaPerfil = $rotaBase . '/perfil';
+
+if ($caminho === $rotaPerfil && $metodo === 'GET') {
+    echo json_encode($perfil);
+} else {
+    http_response_code(404);
+    echo json_encode([
+        "mensagem" => "Recurso não encontrado. Verifique a Rota"
+    ]);
+}
+?>
